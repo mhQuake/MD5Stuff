6e6f4cd483985542b4ad0947daf13485acd795d7
mhQuake <43501276+mhQuake@users.noreply.github.com>
2021-08-25 12:33:55 +0100

    Minimal changes to compile in VC++ 2008

---
+++ clean.cmd
@@ -0,0 +1,22 @@
+attrib *.ncb -r -a -s -h /s /d
+attrib *.suo -r -a -s -h /s /d
+attrib *.user -r -a -s -h /s /d
+
+del *.ncb /s /q
+del *.sdf /s /q
+del *.suo /s /q
+del *.user /s /q
+
+rmdir ipch /s /q
+rmdir Debug /s /q
+rmdir Release /s /q
+
+cd FitzQuake
+
+rmdir Debug /s /q
+rmdir Release /s /q
+
+cd ..
+
+echo done
+pause
